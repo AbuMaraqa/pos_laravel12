be71d7ed783b5c0fd2b19fe332525885fc635cfb
Mohamad Maraqa <maraqamohamad@outlook.sa>
2025-10-31 17:17:37 +0200

    asd

--- app/Livewire/Pages/Product/Edit.php
+++ app/Livewire/Pages/Product/Edit.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Pages\Product;
 
 use App\Enums\InventoryType;
+use App\Models\Inventory;
 use App\Models\Product;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
@@ -776,7 +777,7 @@ class Edit extends Component
             $quantity = abs($difference);
 
             // إنشاء سجل جديد في المخزون
-            \App\Models\Inventory::create([
+            Inventory::create([
                 'product_id' => Product::where('remote_wp_id',$productId)->first()->id ?? null,
                 'quantity' => $quantity,
                 'type' => $type,
